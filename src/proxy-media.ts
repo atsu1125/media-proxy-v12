e887ec0509819eb521ca7d06f2eee76d329043cf
atsu1125 <atsuchan@atsuchan.page>
2025-12-19 20:39:54 +0900

    feat: add emoji query

--- src/proxy-media.ts
+++ src/proxy-media.ts
@@ -26,14 +26,50 @@ export async function proxyMedia(ctx: Koa.Context) {
 
 		const { mime, ext } = await detectType(path);
 		const isConvertibleImage = isMimeImage(mime, 'sharp-convertible-image');
+		const isAnimationConvertibleImage = isMimeImage(mime, 'sharp-animation-convertible-image');
+		const webpDefault: sharp.WebpOptions = {
+			quality: 85,
+			alphaQuality: 95,
+			lossless: false,
+			nearLossless: false,
+			smartSubsample: true,
+			mixed: true,
+		};
 
 		let image: IImage;
 
-		if ('static' in ctx.query && isConvertibleImage) {
+		if ('emoji' in ctx.query && isConvertibleImage) {
+			if (!isAnimationConvertibleImage && !('static' in ctx.query)) {
+				image = {
+					data: fs.readFileSync(path),
+					ext,
+					type: mime,
+				};
+			} else {
+				const data = await sharp(path, { animated: !('static' in ctx.query) })
+				.resize({
+					height: 128,
+					withoutEnlargement: true,
+				})
+				.webp(webpDefault)
+				.toBuffer();
+
+				image = {
+					data,
+					ext: 'webp',
+					type: 'image/webp',
+				};
+			}
+		} else if ('static' in ctx.query && isConvertibleImage) {
 			image = await convertToWebp(path, 498, 280);
 		} else if ('preview' in ctx.query && isConvertibleImage) {
 			image = await convertToWebp(path, 200, 200);
 		} else if ('badge' in ctx.query) {
+			if (!isConvertibleImage) {
+				// 画像でないなら404でお茶を濁す
+				throw new StatusError('Unexpected mime', 404);
+			}
+			
 			const mask = sharp(path)
 				.resize(96, 96, {
 					fit: 'inside',
